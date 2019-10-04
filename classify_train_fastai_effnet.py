bad5da217496152e0e310f8fc647ac2304c23326
chengjiun <chengjiun.ma@gmail.com>
2019-10-04 15:46:25 +0800

    update fastai training code

--- classify_train_fastai_effnet.py
+++ classify_train_fastai_effnet.py
@@ -4,12 +4,6 @@
 # In[1]:
 
 
-get_ipython().run_line_magic('cd', '~/Workspace/kaggle-2019Q3-steel/')
-
-
-# In[2]:
-
-
 import os
 import cv2
 import pdb
@@ -28,7 +22,7 @@ import torch.optim as optim
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader, Dataset, sampler
 from matplotlib import pyplot as plt
-from albumentations import (HorizontalFlip, ShiftScaleRotate, 
+from albumentations import (HorizontalFlip, ShiftScaleRotate,
                             VerticalFlip, Normalize, Resize, Compose, GaussNoise)
 from albumentations.torch import ToTensor
 warnings.filterwarnings("ignore")
@@ -61,7 +55,7 @@ seed_everything()
 
 
 from pathlib import Path
-DATA_BASE_PATH = Path('../DATA/kaggle-2019Q3-steel/') 
+DATA_BASE_PATH = Path('../DATA/kaggle-2019Q3-steel/')
 sample_submission_path = str(DATA_BASE_PATH / 'sample_submission.csv')
 train_df_path = str(DATA_BASE_PATH / 'train.csv')
 data_folder = str(DATA_BASE_PATH)
@@ -89,7 +83,15 @@ from fastai.vision import get_transforms, ImageList, FloatList
 from fastai.vision import cnn_learner, Learner
 from optim.ranger import Ranger
 from fastai.core import partial
-from fastai.metrics import accuracy_thresh, accuracy
+from torch import Tensor
+def accuracy_thresh(y_pred, y_true, thresh:float=0.5, sigmoid:bool=False):
+    "Computes accuracy when `y_pred` and `y_true` are the same size."
+    if sigmoid: y_pred = y_pred.sigmoid()
+    y_pred = np.array(y_pred).flatten()
+    y_true = np.array(y_true).flatten()
+    return Tensor([((y_pred>thresh)==y_true).mean()])
+
+
 optar = partial(Ranger)
 
 
@@ -99,41 +101,18 @@ optar = partial(Ranger)
 CV=1
 seed = CV
 bs = 20
-tfms = get_transforms(flip_vert=True, do_flip=True, max_zoom=1.05, max_lighting=0.2, 
+tfms = get_transforms(flip_vert=True, do_flip=True, max_zoom=1.05, max_lighting=0.2,
                       max_warp=0.05, max_rotate=5.)
-data = (ImageList.from_df(df=image_df,path=DATA_BASE_PATH / 'train_images',cols='ImageId') 
-        .split_from_df() 
-        .label_from_df(cols='Detected',label_cls=FloatList) 
-        .transform(tfms) 
-        .databunch(bs=bs,num_workers=4) 
-        .normalize(IMAGE_STATS_GLOBAL2)  
+data = (ImageList.from_df(df=image_df,path=DATA_BASE_PATH / 'train_images',cols='ImageId')
+        .split_from_df()
+        .label_from_df(cols='Detected',label_cls=FloatList)
+        .transform(tfms)
+        .databunch(bs=bs,num_workers=4)
+        .normalize(IMAGE_STATS_GLOBAL2)
        )
 
 
-# In[21]:
-
-
-data.get(5)
-
-
-# In[13]:
-
-
-data.get(5).shape
-
-
-# In[13]:
-
-
-
-data.show_batch(3, row=bs, figsize=(6,6), hide_axis=False)
-
-
-# # model
-
-# In[22]:
-
-
+from fastai.vision.models import resnet50
 from models.efficientnet import EfficientNet
 #making model
 arch = 'efficientnet-b0'
@@ -141,38 +120,12 @@ model_name = f'{arch}-v1'
 # Parameters for the entire model (stem, all blocks, and head)
 
 md_ef = EfficientNet.from_pretrained(arch, num_classes=1, dropout_rate=0.5)
-
-
-# In[26]:
-
+# md_ef = resnet50(pretrained=False, num_classes=1)
 
 learn = Learner(data, md_ef, opt_func=optar,
-                metrics = [accuracy_thresh], 
+                metrics = [accuracy_thresh],
                 model_dir='fastai-class1').to_fp16()
 learn.path = Path(DATA_BASE_PATH)
-
-
-# In[35]:
-
-
-learn.model
-
-
-# In[24]:
-
-
-learn.unfreeze()
-learn.lr_find()
-learn.recorder.plot(suggestion=True)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 # First
@@ -181,16 +134,10 @@ learn.fit_one_cycle(10, max_lr=1e-2)
 learn.save(f'{model_name}')
 
 
-# In[ ]:
-
-
 # First
 learn.unfreeze()
 learn.fit_one_cycle(10, max_lr=1e-3)
 learn.save(f'{model_name}-stage2')
-
-
-# In[ ]:
 
 
 # First
